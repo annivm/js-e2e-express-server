aacf0d23b07ce61070ab576e49894cb6a9476d8f
Anni Mustonen <154229132+annivm@users.noreply.github.com>
2026-02-10 12:36:53 +0200

    Change port environment variable to WEB_PORT

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const server = require('./server');
 
 // App Service on Windows: PORT
 // App Service on Linux: WEB_PORT
-const port = process.env.PORT || 3000;
+const port = process.env.WEB_PORT || 3000;
 const hostname = '0.0.0.0';
 
 server.create()
@@ -10,4 +10,5 @@ server.create()
         app.listen(port, hostname, () => {
             console.log(`Server has started on port ${port}!`);
         });
+
     }).catch(err => console.log(err));
